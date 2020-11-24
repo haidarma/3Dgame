a7740893e9c427d954510d8d42d7bd9438d2ff7d
haidarma <haidarma@umich.edu>
2020-11-24 17:13:10 -0500

    First person view + movement
    First person view + movement added this includes jumping.

---
+++ Nightmare Fuel/Assets/Scripts/playerView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerView : MonoBehaviour
+{
+    
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotate = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //We want to lock our cursor in the middle
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRotation();
+    }
+
+    //Rotate the player body and camera
+    void playerRotation()
+    {
+        //Get the current value of where the mouse is and place it in variable
+        //Multiply it by mouseSensitivity for movement
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotate -= mouseY;
+
+        //to avoid over rotation
+        xRotate = Mathf.Clamp(xRotate, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
